a4beda48d503f717923f607b30846071945bb2da
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-25 08:03:01 -0400

    Edit User seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,23 @@
+require 'faker'
 require 'httparty'
 require 'urlify'
 accessKey = ENV['UNSPLASH_ACCESS_KEY']
 Images = []
 
-User.create([
-  {
-    sub: "1",
-    email: "ryelbanfield@gmail.com",
-    name: "Ryel Banfield",
-    given_name: "Ryel",
-    family_name: "Banfield",
-    picture: "https://picture.jpg"
-  },
-  {
-    sub: "2",
-    email: "johndoe@gmail.com",
-    name: "John Doe",
-    given_name: "John",
-    family_name: "Doe",
-    picture: "https://picture.jpg"
-  }
-])
+25.times do
+  name = Faker::Name.name
+  User.create(
+    {
+      sub: Faker::Internet.uuid,
+      email: Faker::Internet.email,
+      name: name,
+      given_name: name.split(' ')[0],
+      family_name: name.split(' ')[1],
+      picture: "https://avatars.dicebear.com/api/initials/#{name}.svg"
+    }
+  )
+end
+
 
 countriesResponse = HTTParty.get('https://restcountries.eu/rest/v2/region/europe')
 countriesJSON = countriesResponse.parsed_response
@@ -35,14 +32,14 @@ countriesJSON.each do |country|
     flag: country['flag']
   )
 
-  Images.concat(HTTParty.get("https://api.unsplash.com/search/photos/?client_id=#{accessKey}&query=#{URLify.urlify(country['capital'], '-')}&per_page=5&orientation=landscape").parsed_response['results'] ? HTTParty.get("https://api.unsplash.com/search/photos/?client_id=#{accessKey}&query=#{URLify.urlify(country['capital'], '-')}&per_page=5&orientation=landscape").parsed_response['results'] : [])
+  # Images.concat(HTTParty.get("https://api.unsplash.com/search/photos/?client_id=#{accessKey}&query=#{URLify.urlify(country['capital'], '-')}&per_page=5&orientation=landscape").parsed_response['results'] ? HTTParty.get("https://api.unsplash.com/search/photos/?client_id=#{accessKey}&query=#{URLify.urlify(country['capital'], '-')}&per_page=5&orientation=landscape").parsed_response['results'] : [])
 
 
-  Images.each do |image|
-    Image.create(
-      country_id: Country.last.id,
-      url: image['urls']['regular']
-    )
-  end
-  Images.clear
+  # Images.each do |image|
+  #   Image.create(
+  #     country_id: Country.last.id,
+  #     url: image['urls']['regular']
+  #   )
+  # end
+  # Images.clear
 end

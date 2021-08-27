8d441db360bcbfcfcf21ba7489146a494f78f7d8
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-27 12:39:05 -0400

    Edit validation

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,5 +2,5 @@ class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :country
 
-  validates :user_id, :country_id, presence: true, uniqueness: { scope: :country_id }
+  validates :user_id, presence: true, uniqueness: { scope: :country_id }
 end

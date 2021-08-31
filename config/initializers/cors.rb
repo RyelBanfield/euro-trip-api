ce8f211a6b9f87b81e210c80e83f0ec049158ac1
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-31 09:39:24 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ryelbanfield.github.io'
+    origins 'hhttps://euro-trip.vercel.app/'
 
     resource '*',
       headers: :any,

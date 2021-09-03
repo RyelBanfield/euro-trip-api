6f423ecff6c634d15aaaf858c42a7bdf972e55ec
Ryel Banfield <ryelbanfield@gmail.com>
2021-09-03 09:12:33 -0400

    Remove old tests

--- spec/routing/favourites_routing_spec.rb
+++ spec/routing/favourites_routing_spec.rb
@@ -6,20 +6,8 @@ RSpec.describe FavouritesController, type: :routing do
       expect(get: '/favourites').to route_to('favourites#index')
     end
 
-    it 'routes to #show' do
-      expect(get: '/favourites/1').to route_to('favourites#show', id: '1')
-    end
-
     it 'routes to #create' do
       expect(post: '/favourites').to route_to('favourites#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: '/favourites/1').to route_to('favourites#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: '/favourites/1').to route_to('favourites#update', id: '1')
     end
 
     it 'routes to #destroy' do
